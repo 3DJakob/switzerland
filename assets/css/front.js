69a06f637461201d2b84e92aac2ea2309d4b9a01
Jakob Unnebäck <jakob@folkdatorn.se>
2016-11-18 22:37:33 +0100

    Added new content

--- assets/css/front.js
+++ assets/css/front.js
@@ -9,6 +9,7 @@ export default StyleSheet.create({
     "home divcontent": {
         "backgroundImage": "url(../img/mountains.jpg)",
         "backgroundSize": "cover",
+        "backgroundPosition": "center",
         "minHeight": 100 * vh,
         "minWidth": 100 * vw,
         "display": "flex",
@@ -30,6 +31,15 @@ export default StyleSheet.create({
     },
     "home divcontent:nth-child(4)": {
         "backgroundImage": "url(../img/city.jpg)"
+    },
+    "home divcontent:nth-child(5)": {
+        "backgroundImage": "url(../img/crane.jpg)"
+    },
+    "home divcontent:nth-child(6)": {
+        "backgroundImage": "url(../img/water.jpg)"
+    },
+    "home divcontent:nth-child(7)": {
+        "backgroundImage": "url(../img/plane.jpg)"
     },
     "home content h1": {
         "color": "#fff"
@@ -62,5 +72,40 @@ export default StyleSheet.create({
     },
     "home content textbox p:last-child": {
         "marginBottom": 0
+    },
+    "home content textbox pending": {
+        "textAlign": "center"
+    },
+    "home content textbox iframemap": {
+        "height": 450,
+        "width": "100%"
+    },
+    "home content textbox divcollage": {
+        "display": "flex",
+        "justifyContent": "center",
+        "flexFlow": "wrap",
+        "marginTop": 40,
+        "marginRight": 0,
+        "marginBottom": 40,
+        "marginLeft": 0
+    },
+    "home content textbox collage img": {
+        "width": 180,
+        "border": "5px solid #fff",
+        "boxShadow": "0px 0px 30px #aaa",
+        "transform": "rotate(5deg)"
+    },
+    "home content textbox collage img:nth-child(2)": {
+        "transform": "rotate(-8deg)"
+    },
+    "home content textbox collage img:nth-child(3)": {
+        "transform": "rotate(-3deg)"
+    },
+    "home content textbox collage img:nth-child(4)": {
+        "transform": "rotate(-7deg)"
+    },
+    "home content textbox iframevideo": {
+        "height": 337,
+        "width": "100%"
     }
 });
